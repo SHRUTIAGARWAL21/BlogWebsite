7c055494f406a9b4680b190e3dd9aa1c382f2cf9
shrutiagarwal21 <khushiagarwal0204@gmail.com>
2025-05-08 01:10:03 +0530

    fixes

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://blog-website-seven-gamma.vercel.app/",
+    origin: "https://blog-website-seven-gamma.vercel.app",
   })
 );
 app.use(cookieParser());
